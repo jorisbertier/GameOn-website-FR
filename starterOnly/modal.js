e5ff6e2f6d1a5a44598eef837120f60aa3bad634
Joris Bertier <joris.bertier@gmail.com>
2024-05-06 07:24:08 -0500

    #1 close modal

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,6 +11,7 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const modalClose = document.querySelector('.close');
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -19,5 +20,12 @@ modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 function launchModal() {
   modalbg.style.display = "block";
 }
+// close modal form
 
+function modalCloseForm(){
+  modalbg.style.display = "none";
+}
+modalClose.addEventListener('click', ()=> {
+  modalCloseForm()
+})
 
