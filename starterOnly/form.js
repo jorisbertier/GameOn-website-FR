2564cfa9db4df26da07064b11b9c0c48fce46bc8
Joris Bertier <joris.bertier@gmail.com>
2024-05-06 08:24:23 -0500

    condition quantity

--- starterOnly/form.js
+++ starterOnly/form.js
@@ -3,6 +3,7 @@ let form = document.querySelector('form')
 let inputName = document.querySelector('#first')
 let inputSurname = document.querySelector('#last')
 let inputEmail = document.querySelector('#email')
+let inputQuantity = document.querySelector('#quantity')
 
 function Validate() {
 
@@ -10,6 +11,7 @@ function Validate() {
 form.addEventListener('submit', (event)=> {
     event.preventDefault()
     let regexEmail = new RegExp('^[^\\s@]+@[^\\s@]+\\.[^\\s@]+$');
+    const regexNumber = /^\d+$/;
 
     // if(inputName.value.trim().length < 2) {
     //     console.log("Le prénom doit comporter au moins 2 caractères")
@@ -19,6 +21,10 @@ form.addEventListener('submit', (event)=> {
     // }
     if(!regexEmail.test(inputEmail.value)) {
         console.log("email non valide")
+    }
+
+    if(!regexNumber.test(inputQuantity.value)) {
+        console.log('Uniquement nombre')
     }
     
 })
