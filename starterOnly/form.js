d9d72560e05bd987f0b7bfdb86608d4beeea4fea
Joris Bertier <joris.bertier@gmail.com>
2024-05-06 08:20:57 -0500

    condition email

--- starterOnly/form.js
+++ starterOnly/form.js
@@ -2,18 +2,23 @@
 let form = document.querySelector('form')
 let inputName = document.querySelector('#first')
 let inputSurname = document.querySelector('#last')
+let inputEmail = document.querySelector('#email')
 
 function Validate() {
 
 }
 form.addEventListener('submit', (event)=> {
     event.preventDefault()
+    let regexEmail = new RegExp('^[^\\s@]+@[^\\s@]+\\.[^\\s@]+$');
 
-    if(inputName.value.trim().length < 2) {
-        console.log("non")
-    }
-    if(inputSurname.value.trim().length < 2) {
-        console.log("non")
+    // if(inputName.value.trim().length < 2) {
+    //     console.log("Le prénom doit comporter au moins 2 caractères")
+    // }
+    // if(inputSurname.value.trim().length < 2) {
+    //     console.log("Le nom doit comporter au moins 2 caractères")
+    // }
+    if(!regexEmail.test(inputEmail.value)) {
+        console.log("email non valide")
     }
     
 })
