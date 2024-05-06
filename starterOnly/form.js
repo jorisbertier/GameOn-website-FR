50073877b819c14bc472ddbda81c05c91a6b823a
Joris Bertier <joris.bertier@gmail.com>
2024-05-06 10:12:59 -0500

    #issue 2 validation field form

--- starterOnly/form.js
+++ starterOnly/form.js
@@ -6,58 +6,93 @@ let inputEmail = document.querySelector('#email')
 let inputQuantity = document.querySelector('#quantity')
 
 
-
-function validateName(name) {
+// Validate name & surname form
+function validateName(name, fieldName) {
     if(name.value.trim().length < 2) {
-        console.log("Le prénom doit comporter au moins 2 caractères")
+        console.log(`Le ${fieldName}  doit comporter au moins 2 caractères`)
+        return false
+    }
+}
+
+// Validate Email form
+function validateEmail(email) {
+    let regexEmail = new RegExp('^[^\\s@]+@[^\\s@]+\\.[^\\s@]+$');
+    if(!regexEmail.test(email.value)) {
+        console.log("email non valide")
+        return false
+    }
+}
+
+//Validate quantity form
+function validateQuantity(quantity) {
+    let regexNumber = /^\d+$/;
+    if(!regexNumber.test(quantity.value)) {
+        console.log('Uniquement nombre')
+        return false
+    }
+}
+
+//Validate selected option form
+function validateSelectedOption(selected) {
+    if (selected) {
+        console.log(selected);
+        return;
+    }
+    else {
+        console.log("Aucune option sélectionnée");
+        return;
+    }
+}
+
+//Validate checkbox check form
+function validateCheckbox(checkbox) {
+    if(checkbox) {
+        console.log("check 1 valid")
+        return
+    } else {
+        console.log("no check 1 invalid")
+        return false
+    }
+}
+
+// Recover checkbox2 check
+function validateCheckbox2(checkbox) {
+    if(checkbox) {
+        console.log("check 2 valid")
+        return
+    } else {
+        console.log("no check 2 invalid")
         return
     }
 }
 
-function validateSurname(surname) {
-    if(surname.value.trim().length < 2) {
-        console.log("Le nom doit comporter au moins 2 caractères")
-    }
-}
 // Tous les champs de validation du formulaire
 function Validate() {
-
-}
-
-form.addEventListener('submit', (event)=> {
-    event.preventDefault()
-    // let regexEmail = new RegExp('^[^\\s@]+@[^\\s@]+\\.[^\\s@]+$');
-    // const regexNumber = /^\d+$/;
-
-    // let inputSelectedOption = document.querySelector('input[name="location"]:checked').value
+    let inputSelectedOption = document.querySelector('input[name="location"]:checked')
     let checkbox1 = document.querySelector('#checkbox1').checked
     let checkbox2 = document.querySelector('#checkbox2').checked
-    
 
-    // if(inputName.value.trim().length < 2) {
-    //     console.log("Le prénom doit comporter au moins 2 caractères")
-    // }
-    validateName(inputName)
-    validateSurname(inputSurname)
-    // if(inputSurname.value.trim().length < 2) {
-    //     console.log("Le nom doit comporter au moins 2 caractères")
-    // }
-    // if(!regexEmail.test(inputEmail.value)) {
-    //     console.log("email non valide")
-    // }
+    validateName(inputName, "prénom")
+    validateName(inputSurname, "nom")
+    validateEmail(inputEmail)
+    validateQuantity(inputQuantity)
 
-    // if(!regexNumber.test(inputQuantity.value)) {
-    //     console.log('Uniquement nombre')
-    // }
-    // if(checkbox1) {
-    //     console.log("check 1 valid")
-    // } else {
-    //     console.log("no check 1")
-    // }
+    if (inputSelectedOption) {
+        console.log(inputSelectedOption.value);
+    } else {
+        console.log("Aucune option sélectionnée");
+    }
 
-    // if(checkbox2) {
-    //     console.log("check 2 valid")
-    // } else {
-    //     console.log("no check 2")
-    // }
+    validateCheckbox(checkbox1)
+    validateCheckbox2(checkbox2)
+
+    if(validateName && validateEmail && validateQuantity && validateCheckbox) {
+        alert('formulaire envoyer')
+    }
+}
+
+//Ecouter event form submit
+form.addEventListener('submit', (event)=> {
+    event.preventDefault()
+    Validate()
 })
