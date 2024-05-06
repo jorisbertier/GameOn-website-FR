a48b41b3338a8170f1157675281f3c1182727cda
Joris Bertier <joris.bertier@gmail.com>
2024-05-06 08:14:03 -0500

    function surname

--- starterOnly/form.js
+++ starterOnly/form.js
@@ -1,6 +1,7 @@
 
 let form = document.querySelector('form')
 let inputName = document.querySelector('#first')
+let inputSurname = document.querySelector('#last')
 
 function Validate() {
 
@@ -9,6 +10,9 @@ form.addEventListener('submit', (event)=> {
     event.preventDefault()
 
     if(inputName.value.trim().length < 2) {
+        console.log("non")
+    }
+    if(inputSurname.value.trim().length < 2) {
         console.log("non")
     }
     
